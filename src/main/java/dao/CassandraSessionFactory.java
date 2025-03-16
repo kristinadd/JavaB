751860192cc8e119404e0bc600cb56d81e1a5dd6
kristinadd <kristina@productboard.com>
2025-03-16 10:26:10 +0100

    Refactor CassandraConnection to use singleton

---
+++ src/main/java/dao/CassandraSessionFactory.java
@@ -0,0 +1,37 @@
+package dao;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Properties;
+
+public class CassandraSessionFactory {
+    private static CassandraSessionFactory instance = new CassandraSessionFactory("db.properties");
+    private Properties props;
+
+    private CassandraSessionFactory(String fname) {
+        props = new Properties();
+        try {
+            props.load(getClass().getClassLoader().getResourceAsStream(fname));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static CassandraSessionFactory instance() {
+        return instance;
+    }
+
+    public CqlSession getSession() {
+        String host = props.getProperty("CASSANDRA_HOST", "localhost");
+        int port = Integer.parseInt(props.getProperty("CASSANDRA_PORT", "9042"));
+        String datacenter = props.getProperty("CASSANDRA_DATACENTER", "datacenter1");
+        // String keyspace = props.getProperty("CASSANDRA_KEYSPACE", "javabank");
+
+        return CqlSession.builder()
+                .addContactPoint(new InetSocketAddress(host, port))
+                .withLocalDatacenter(datacenter)
+                // .withKeyspace(keyspace) --> specific keyspace
+                .build();
+    }
+}
--- src/main/java/dao/CassandraTestInsert.java
+++ src/main/java/dao/CassandraTestInsert.java
@@ -3,23 +3,18 @@ package dao;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.*;
 import java.math.BigDecimal;
-import java.net.InetSocketAddress;
 import java.time.Instant;
 import java.util.UUID;
 
 public class CassandraTestInsert {
     public static void main(String[] args) {
-        // Connect to Cassandra using the keyspace "javabank"
-        try (CqlSession session = CqlSession.builder()
-                .withKeyspace("javabank")
-                .addContactPoint(new InetSocketAddress("localhost", 9042))
-                .withLocalDatacenter("datacenter1")
-                .build()) {
-
+        // Obtain a CqlSession using our CassandraSessionFactory singleton
+        try (CqlSession session = CassandraSessionFactory.instance().getSession()) {
             System.out.println("Connected to Cassandra!");
 
-            // Create the table "account" if it doesn't already exist.
-            String createTableQuery = "CREATE TABLE IF NOT EXISTS account (" +
+            // Create the table "account" in the 'javabank' keyspace if it doesn't already exist.
+            // Here we use the fully qualified table name (keyspace.table) since the session may not have a default keyspace.
+            String createTableQuery = "CREATE TABLE IF NOT EXISTS javabank.account (" +
                                       "account_id UUID PRIMARY KEY, " +
                                       "account_holder text, " +
                                       "balance decimal, " +
@@ -29,21 +24,21 @@ public class CassandraTestInsert {
             System.out.println("Table 'account' created (if it did not exist).");
 
             // Insert a record into the "account" table using a prepared statement.
-            String insertQuery = "INSERT INTO account (account_id, account_holder, balance, opened_date) " +
+            String insertQuery = "INSERT INTO javabank.account (account_id, account_holder, balance, opened_date) " +
                                  "VALUES (?, ?, ?, ?);";
             PreparedStatement preparedInsert = session.prepare(insertQuery);
             UUID accountId = UUID.randomUUID();
             BoundStatement boundInsert = preparedInsert.bind(
                     accountId,
-                    "Test Account",
-                    BigDecimal.valueOf(1234.56),
+                    "Test Account 2",
+                    BigDecimal.valueOf(999.56),
                     Instant.now()
             );
             session.execute(boundInsert);
             System.out.println("Record inserted with account_id: " + accountId);
 
             // Optionally, query the record to verify it was inserted.
-            String selectQuery = "SELECT account_id, account_holder, balance, opened_date FROM account WHERE account_id = ?;";
+            String selectQuery = "SELECT account_id, account_holder, balance, opened_date FROM javabank.account WHERE account_id = ?;";
             PreparedStatement preparedSelect = session.prepare(selectQuery);
             BoundStatement boundSelect = preparedSelect.bind(accountId);
             ResultSet resultSet = session.execute(boundSelect);
